fbd4b17e7252003d12bd2dce931717c2b7abec63
Yogic <yogicrhama@gmail.com>
2021-11-18 21:51:43 +0700

    Able to create monster

--- main.go
+++ main.go
@@ -7,4 +7,6 @@ import (
 func main() {
 	var hero *Hero = CreateHero("HERO", 1000, 50)
 	var hero2 *Hero = CreateHero("HERO2", 1000, 50)
+	var monster *Monster = CreateMonster("Monster", 100, 20)
+	var monster2 *Monster = CreateMonster("Monster2", 100, 20)
 }
