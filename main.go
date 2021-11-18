8d1414a0de6c2118a87c0f5fdb85f22b534bfd66
Yogic <yogicrhama@gmail.com>
2021-11-18 21:52:00 +0700

    Able to create soldier

--- main.go
+++ main.go
@@ -9,4 +9,6 @@ func main() {
 	var hero2 *Hero = CreateHero("HERO2", 1000, 50)
 	var monster *Monster = CreateMonster("Monster", 100, 20)
 	var monster2 *Monster = CreateMonster("Monster2", 100, 20)
+	var soldier *Soldier = CreateSoldier("Soldier", 50, 10)
+	var soldier2 *Soldier = CreateSoldier("Soldier2", 50, 10)
 }
